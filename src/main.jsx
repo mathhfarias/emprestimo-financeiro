00adc9f8bdbd4adadf646eca6b307c79a70843a7
Matheus Farias <matheusfariasdelima@gmail.com>
2026-05-12 22:33:09 -0300

    fix: wrap routes with browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { AuthProvider } from './contexts/AuthContext.jsx';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
